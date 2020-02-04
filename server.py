8bb11691c09ec5e8d95af5d84eb4c3b70368a622
Péter Pál Vanyó <vanyo.peter.pal@gmail.com>
2020-02-04 15:53:23 +0100

    using the template

--- server.py
+++ server.py
@@ -1,14 +1,28 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/<username>/<int:id>')
-def hello_world(username, id=None):
-    return render_template("index.html", username=username, id=id)
+@app.route('/')
+def my_home():
+    return render_template("index.html")\
 
-@app.route("/about")
+@app.route('/index.html')
+def home():
+    return render_template("index.html")
+
+
+@app.route("/works.html")
+def works():
+    return render_template("works.html")
+
+@app.route('/about.html')
 def about():
     return render_template("about.html")
 
-@app.route("/blog")
-def blog():
-    return "this is my blog!!!!!"
+@app.route('/contact.html')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/components.html')
+def components():
+    return render_template("components.html")
+
