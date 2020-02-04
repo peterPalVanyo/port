2f307f2ae36c34523ae38a88e311909253fc6452
Péter Pál Vanyó <vanyo.peter.pal@gmail.com>
2020-02-04 11:08:40 +0100

    using jinja

--- server.py
+++ server.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, intersection! (kansas won)'
+@app.route('/<username>/<int:id>')
+def hello_world(username, id=None):
+    return render_template("index.html", username=username, id=id)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/blog")
+def blog():
+    return "this is my blog!!!!!"
