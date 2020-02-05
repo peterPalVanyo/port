ef5fcd3f8dd8184491e3e1cab658ac2853803015
Péter Pál Vanyó <vanyo.peter.pal@gmail.com>
2020-02-05 05:52:18 +0100

    txt as database

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 @app.route('/')
@@ -10,6 +10,23 @@ def my_home():
 def page(page):
     return render_template(page)
 
+
+@app.route('/submit', methods=['POST', 'GET'])
+def submit():
+    if request.method == "POST":
+        data = request.form.to_dict()
+        write_to_file(data)
+        return redirect("thankyou.html")
+    else:
+        return "something went wrong"
+
+
+def write_to_file(data):
+    with open("database.txt", mode="a") as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        file = database.write(f"\n{email},{subject},{message}")
 
 # @app.route('/components.html')
 # def components():
